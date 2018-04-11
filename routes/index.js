79b5e275ecf44bf0852ece312d20e68f81065d55
Tobias Hjertelundh <tobias.hjertelundh@hotmail.com>
2018-04-11 13:27:29 +0200

    db connected and insert members works

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,23 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/register', function(req, res, next) {
+  res.render('register', { title: 'Registrering' });
+});
+
+router.post('/register', function(req, res, next) {
+  const name = req.body.name;
+  const email = req.body.email;
+  const password = req.body.password;
+  
+  
+
+  const db = require('../db.js');
+  db.query('INSERT INTO users (name, email, password) VALUES (?, ?, ?)', [name, email, password], function(error, results, fields) {
+    if(error) throw error;
+    res.render('register', { title: 'Registrering genomförd' });
+  });
+  
 });
 
 module.exports = router;
